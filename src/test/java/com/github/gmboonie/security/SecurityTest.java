ea404bdab8ccae7f6c30cb496a10a657dfd19a80
Administrator <Administrator@CQ-201409091313>
2014-11-24 14:09:21 +0800

    增加新的测试文件

---
+++ src/test/java/com/github/gmboonie/security/SecurityTest.java
@@ -0,0 +1,20 @@
+package com.github.gmboonie.security;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 测试com.github.gmboonie.security 下各种加密解密方式。
+ * @author Administrator
+ *
+ */
+public class SecurityTest {
+	
+	//TODO 加密解密测试
+	@Test
+	public void test() {
+		
+	}
+
+}
